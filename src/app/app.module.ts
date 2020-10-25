b497f6e602aceb67959f2acc8c08a99204cbbafc
Yiyun Zheng <yzheng63@ucsc.edu>
2020-10-24 23:28:58 -0700

    update layout'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { MainComponent } from './layout/main/main.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderTopComponent } from './layout/header-top/header-top.component';
+import { HeaderNavComponent } from './layout/header-nav/header-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    HeaderTopComponent,
+    HeaderNavComponent
   ],
   imports: [
     BrowserModule,
