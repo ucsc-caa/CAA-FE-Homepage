4c5a5f63452746e80378b815c5d4e96251f4e4d8
jiayin <jliu240@ucsc.edu>
2020-11-01 11:22:17 +0800

    add footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
-
+  public picUrl = 'https://media-exp1.licdn.com/dms/image/C510BAQEIrV2s0A7o6w/company-logo_200_200/0?e=2159024400&v=beta&t=vMh56jL2BOYKMFZYHXigSCkQ6VfT8bVK3OcnFTiwwPU';
   constructor() { }
 
   ngOnInit(): void {
