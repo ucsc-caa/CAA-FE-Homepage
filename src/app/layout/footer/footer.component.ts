d4611b5ad695aa11c46100df390523fea1411277
Yiyun Zheng <yzheng63@ucsc.edu>
2020-11-07 22:59:38 -0800

    Fix styles

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -12,17 +12,17 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  @HostListener("document:scroll") changeFooter(){
-    let footerNav = document.getElementById("footerNav");
-    if(document.body.scrollTop>0||document.documentElement.scrollTop>0){
-      footerNav.style.height = "80px";
-      footerNav.style.lineHeight = "80px";
+  // @HostListener("document:scroll") changeFooter(){
+  //   let footerNav = document.getElementById("footerNav");
+  //   if(document.body.scrollTop>0||document.documentElement.scrollTop>0){
+  //     footerNav.style.height = "80px";
+  //     footerNav.style.lineHeight = "80px";
 
-    } else {
-      footerNav.style.height = "25px";
-      footerNav.style.lineHeight = "25px";
+  //   } else {
+  //     footerNav.style.height = "25px";
+  //     footerNav.style.lineHeight = "25px";
 
-    }
+  //   }
     
-  }
+  // }
 }
