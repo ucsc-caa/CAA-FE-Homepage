3d1e5d6ee86474cbcf7dc519ae62fb4e5a1a946f
Peter Cai <caiyongmao0420@gmail.com>
2020-11-17 11:59:59 +0800

    text data type

--- src/app/layout/header-nav/header-nav.component.ts
+++ src/app/layout/header-nav/header-nav.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./header-nav.component.css']
 })
 export class HeaderNavComponent implements OnInit {
+  
+  public give = ['捐赠','Give']
+  public newscenter = ['新闻中心','Newscenter']
+  public connect = ['校友联络','Connect']
+  public community = ['校友社区','Community']
+  public merchandise = ['商品','Merchandise']
+  public membership = ['会员','Membership']
+  public about = ['关于','About']
+  public join = ['加入','Join']
 
   constructor() { }
 
--- src/app/layout/header-top/header-top.component.ts
+++ src/app/layout/header-top/header-top.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit, HostListener} from '@angular/core';
 })
 export class HeaderTopComponent implements OnInit {
 
+  public getinvolved = ['志愿机会','Get Involved']
+  public career = ['工作机会','Career']
+  public immigration = ['移民咨询','Immigration']
+
   constructor() { }
 
   ngOnInit(): void {
