7e67c8507b56bf870fc195d20ae8f75a70bc8eec
jiayin <jliu240@ucsc.edu>
2020-12-10 22:16:30 +0800

    change language for footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -4,10 +4,14 @@
  * @author: Jiayin Liu
  * Revised: 11/8/2020 add comments.
  *
+ * 
+ * Revised by Jiayin Liu on 12/10/2020.
+ * Changing language
  */
-import { Component, OnInit, HostListener, Input } from '@angular/core';//
-import {PageInfoService} from '../../services/page-info.service';//
-import { text } from '../../models/text';//
+import { Component, OnInit, HostListener, Input } from '@angular/core';
+import {PageInfoService} from '../../services/page-info.service';
+import { text } from '../../models/text';
+
 
 @Component({
   selector: 'app-footer',
@@ -25,15 +29,21 @@ export class FooterComponent implements OnInit {
 
   ngOnInit(): void {
 
-    //--
+   
     this.langs = {
-      a:{CN:'关于', EN:'A'},
-      b:{CN:'CAA管理层',EN:'B'},
-      c:{CN:'新闻中心',EN:'C'},
-      d:{CN:'隐私政策',EN:'D'},
-      e:{CN:'使用条款', EN:'E'},
+      a:{CN:'关于', EN:'About'},
+      b:{CN:'CAA管理层',EN:'Leadership'},
+      c:{CN:'CAA价值观',EN:'Our mission'},
+      d:{CN:'新闻中心',EN:'Newscenter'},
+      e:{CN:'隐私政策',EN:'Privacy Policy'},
+      f:{CN:'使用条款', EN:'Term of use'},
+      //--
+      copyright:{CN:'2020 UCSC Chinese Alumni Association. 保留所有权利', EN:'2020 UCSC Chinese Alumni Association. All Rights Reserved'},
+      declare:{CN:'加州大学圣克鲁兹分校学生会是一个依赖学生、校友、大学捐赠的非营利性组织', EN:'UCSC CAA is a self-funded nonprofit organization that relies on donations to provide programs and services that support students, alumni, and the University'},
+      //--
+      
     }
-    //--
+  
   }
 
   // @HostListener("document:scroll") changeFooter(){
